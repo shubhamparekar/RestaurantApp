cfe193e17e0419636e8e2b026c74142e17eddc6f
shubhamparekar <49017702+shubhamparekar@users.noreply.github.com>
2022-08-24 20:16:17 +0530

    Add files via upload

--- quasar.config.js
+++ quasar.config.js
@@ -12,7 +12,7 @@
 const { configure } = require('quasar/wrappers');
 
 
-module.exports = configure(function (/* ctx */) {
+module.exports = configure(function ( ctx ) {
   return {
     
 
@@ -52,6 +52,8 @@ module.exports = configure(function (/* ctx */) {
         browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
         node: 'node16'
       },
+
+      distDir: ctx.mode.spa ? 'public' : null,
 
       vueRouterMode: 'hash', // available values: 'hash', 'history'
       // vueRouterBase,
